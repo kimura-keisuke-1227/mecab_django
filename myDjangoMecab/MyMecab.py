08489ee96813a294930e094e824d0335aeae2401
Keisuke Kimura <kim.ksuke@gmail.com>
2024-05-18 15:21:29 +0900

    Mecabの共通処理を担当するクラス

---
+++ myDjangoMecab/MyMecab.py
@@ -0,0 +1,13 @@
+import MeCab
+import jaconv
+import re
+import json
+import MyLogger
+# from RubyDictJsonMaker import RubyDictJsonMaker
+# from MyRubyTools import MyRubyTools
+
+column_text = 'word'
+column_kana = 'ruby'
+column_type1 = 'type1'
+column_type2 = 'type2'
+column_is_end_of_line = 'end_of_line'
