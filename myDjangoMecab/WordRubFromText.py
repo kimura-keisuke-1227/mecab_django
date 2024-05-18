32af508ecb3375f569157cd23c731fa965e9ba0e
Keisuke Kimura <kim.ksuke@gmail.com>
2024-05-18 15:01:34 +0900

    引数を読込

--- myDjangoMecab/WordRubFromText.py
+++ myDjangoMecab/WordRubFromText.py
@@ -15,18 +15,26 @@ class WordRubyFromText:
         self.logger.info("Test Start")
         self.logger.info("Test End")
         
-    def main(self):
+    def main(self,input_file_name,output_file_name):
         self.logger.info("main Start")
         self.test()
         self.logger.info("main End")
 
 if __name__ == "__main__":
-	print('WordRubyFromText Program Start!')
-	psr = argparse.ArgumentParser(
+    print('WordRubyFromText Program Start!')
+    psr = argparse.ArgumentParser(
         prog='ルビ入りWordファイル作成プログラム',
         usage='コマンドラインから引数を指定して実行',
         description='テキストファイルからルビが入ったWordファイルを自動で作成'
     )
-	instance = WordRubyFromText()
-	instance.main()
-	print('WordRubyFromText Program End!')
+    psr.add_argument('-i', '--input', required=True,help='インプットファイル名(拡張子はつけない)')
+    psr.add_argument('-o', '--output', required=True,help='アウトプットファイル名(拡張子はつけない)')
+
+    args = psr.parse_args()
+    
+    input_file_name = args.input
+    output_file_name = args.output
+
+    instance = WordRubyFromText()
+    instance.main(input_file_name,output_file_name)
+    print('WordRubyFromText Program End!')
