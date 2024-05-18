9c3357f4dd367c8124d7321385367e8b2babf5cb
Keisuke Kimura <kim.ksuke@gmail.com>
2024-05-18 14:51:32 +0900

    とりあえず動くように

--- myDjangoMecab/WordRubFromText.py
+++ myDjangoMecab/WordRubFromText.py
@@ -5,5 +5,22 @@ import sys
 sys.path.append('../')
 from MyLogger.MyLogger import MyLogger
 
-logger = MyLogger().logger
-logger.info('test')
+class WordRubyFromText:
+    def __init__(self) -> None:
+        self.logger = MyLogger().logger
+        self.logger.info('test')
+    
+    def test(self):
+        self.logger.info("Test Start")
+        self.logger.info("Test End")
+        
+    def main(self):
+        self.logger.info("main Start")
+        self.test()
+        self.logger.info("main End")
+
+if __name__ == "__main__":
+	print('WordRubyFromText Program Start!')
+	instance = WordRubyFromText()
+	instance.main()
+	print('WordRubyFromText Program End!')
